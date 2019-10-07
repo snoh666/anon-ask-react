d60e855232af63399527bb3b23e94830fecfb2de
Mikolaj Snochowski <snochopl@gmail.com>
2019-10-07 11:31:25 +0200

    Basic structure of maain el

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'
 
-const Main = () => {
+const Main = ({ isAuth }) => {
+
+  const [tell, setTell] = useState('');
+
+  const updateTell = e => {
+    setTell(e.target.value);
+  }
+
+  const sendTell = e => {
+    e.preventDefault();
+
+    // Here you need to send a tell to firebase storage
+  }
 
   return (
     <div>
-      <span>Main UI with tell</span>
+      <header>
+        {isAuth ? (<Link to="/tells">Tells</Link>) : (<Link to="/login">Login</Link>)}
+      </header>
+      <section>
+        <form onSubmit={sendTell}>
+          <textarea cols="30" rows="5" value={tell} onChange={updateTell}></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </section>
     </div>
   );
 }
