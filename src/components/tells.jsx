dcd46e5dcb249b0587afab7890e146547a9c8006
Mikolaj Snochowski <snochopl@gmail.com>
2019-10-07 14:20:11 +0200

    MVP of getting all tells

--- src/components/tells.jsx
+++ src/components/tells.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Tell from './tell';
 
 const Tells = ({ database }) => {
 
+  const [tells, setTells] = useState([]);
   const db = database.firestore().collection('/tells');
+  const currentTime = new Date().getTime();
 
   db.get().then(query => {
+    const tellsArray = [];
     query.forEach((doc) => {
-      console.log(doc.data());
+      tellsArray.push({
+        id: doc.id,
+        ...doc.data()
+      });
     });
+
+    setTells(tellsArray)
   }).catch(err => console.log(err));
 
   return (
     <div>
-      <span>Tells route</span>
+      <h3>Tells</h3>
+      <div>
+        {
+          tells.map(tellData => ( <Tell key={tellData.id} data={tellData} currentTime={currentTime} /> ))
+        }
+      </div>
     </div>
   );
 }
