4f02b3be9254d24445cb14cb4545975b73af5b87
Mikolaj Snochowski <snochopl@gmail.com>
2019-10-07 11:19:54 +0200

    Basic structure of login form and isAuth switch

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom';
 
-const Login = () => {
+const Login = ({ setAuth, isAuth }) => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const tryLogin = e => {
+    e.preventDefault();
+
+    setAuth(true);
+
+  }
 
   return (
     <header>
-      <div>
-        <span>Link to login or export authed username</span>
+      <div className="Login">
+        {isAuth ? (
+          <Redirect to="/" />
+        ) : (
+          <form onSubmit = { tryLogin }>
+            <input type = "text" className = "username"/>
+            <button type="submit">Login</button>
+          </form>
+        )}
       </div>
     </header>
   );
