9000ebcbbd5b8cbbd5a363b62044fa076e4c6157
Mikolaj Snochowski <snochopl@gmail.com>
2019-10-07 17:30:18 +0200

    Deleted tellCheck with border

--- src/components/styled-components/Button.js
+++ src/components/styled-components/Button.js
@@ -4,7 +4,7 @@ const Button = styled.button`
   margin: 10px 0 0;
   padding: .25em 1em;
   background: none;
-  border: 1px solid ${props => props.tellCheck ? 'black' : 'black'};
+  border: 1px solid black;
   display: inline;
   cursor: pointer;
   color: ${props => props.tellCheck ? '#d63031' : props.theme.fontColor}
