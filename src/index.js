f0feca5276c1658814d111c25cf523dc95da9627
Mikolaj Snochowski <snochopl@gmail.com>
2019-10-07 12:25:14 +0200

    Added firebase config initialization

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAsjc-8h32zPZh0qJIBDU1_XSCDxtvhGXE",
+  authDomain: "spotted-staszicv2.firebaseapp.com",
+  databaseURL: "https://spotted-staszicv2.firebaseio.com",
+  projectId: "spotted-staszicv2",
+  storageBucket: "",
+  messagingSenderId: "948345929307",
+  appId: "1:948345929307:web:b846e779c8b6f59778097e",
+  measurementId: "G-7KLNE78VG8"
+};
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+
+ReactDOM.render(<App firebase={firebase} />, document.getElementById('root'));
