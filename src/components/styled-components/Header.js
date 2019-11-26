1865920e3849a1f653770764b920b740ff90563a
Mikolaj Snochowski <snochopl@gmail.com>
2019-11-26 09:26:19 +0100

    Added hyperlink animations

--- src/components/styled-components/Header.js
+++ src/components/styled-components/Header.js
@@ -3,15 +3,30 @@ import styled from 'styled-components';
 const Header = styled.header`
   display: flex;
   width: 100%;
+  max-width: 550px;
   margin: 1em;
   padding: 1em .25em;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
 
   & h2 {
     font-weight: lighter;
     border-bottom: 1px solid ${props => props.theme.fontColor};
+  }
+  & a {
+    text-transform: none;
+    color: inherit;
+    transition: transform .4s ease;
+    &:hover{
+      transform: scale(1.15);
+    }
+    &[href="/login"] {
+      margin-top: 10px;
+      button {
+        margin: 0;
+      }
+    }
   }
 `;
 
