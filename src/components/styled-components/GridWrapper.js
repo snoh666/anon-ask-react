0d3f5c584d4e22c417da664bbf97816846ba747f
Mikolaj Snochowski <snochopl@gmail.com>
2019-11-26 09:35:40 +0100

    Add GridWrapper for tell

---
+++ src/components/styled-components/GridWrapper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const GridWrapper = styled.div`
+  display: grid;
+  margin-top: 20px;
+  grid-template-columns: 1fr 100px 30px;
+  grid-gap: 0 10px;
+`;
+
+export default GridWrapper;
--- src/components/tell.jsx
+++ src/components/tell.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import GridWrapper from './styled-components/GridWrapper';
 
 const Tell = ({data: {tell, time, isAdded}, currentTime}) => {
 
   return (
-    <div>
+    <GridWrapper>
       <div>{tell}</div>
       <div>{Math.floor((currentTime - time) / 1000 / 60) + 'min ago.'}</div>
       <div>{isAdded ? 'true' : 'false'}</div>
-    </div>
+    </GridWrapper>
   );
 }
 
