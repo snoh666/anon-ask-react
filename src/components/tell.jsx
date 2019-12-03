585130fe11fa383f229db46ab8801ccb16f535f0
Mikolaj Snochowski <snochopl@gmail.com>
2019-12-03 22:34:54 +0100

    Added new time function

--- src/components/tell.jsx
+++ src/components/tell.jsx
@@ -3,11 +3,34 @@ import GridWrapper from './styled-components/GridWrapper';
 
 const Tell = ({data: {tell, time, isAdded}, currentTime}) => {
 
+  const getTime = (timeValue) => {
+    let timeAgo = Math.floor((currentTime - timeValue) / 1000 / 60);
+    let suffix = 'min ago.';
+    if(timeAgo >= 60) {
+      timeAgo = Math.floor(timeAgo / 60);
+      suffix = 'hours ago.';
+      if(timeAgo > 24) {
+        timeAgo = Math.floor(timeAgo / 24);
+        suffix = 'days ago.';
+        if(timeAgo > 30) {
+          timeAgo = Math.floor(timeAgo / 30);
+          suffix = 'months ago.';
+        }
+      }
+    }
+
+    return `${timeAgo} ${suffix}`;
+  }
+
   return (
     <GridWrapper>
       <div>{tell}</div>
-      <div>{Math.floor((currentTime - time) / 1000 / 60) + 'min ago.'}</div>
-      <div>{isAdded ? 'true' : 'false'}</div>
+      <div>
+        {getTime(time)}
+      </div>
+      <div>
+        {isAdded ? 'T' : 'F'}
+      </div>
     </GridWrapper>
   );
 }
