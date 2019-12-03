5d9b5faa3cb131e8c4bed0b26d31a326e0744f23
Mikolaj Snochowski <snochopl@gmail.com>
2019-12-03 23:57:41 +0100

    Integrated react redux with provider

--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import firebase from './Firebase';
 
+import { Provider } from 'react-redux';
+import store from './redux';
 
-ReactDOM.render(<App firebase={firebase} />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App firebase={firebase} />
+  </Provider>,
+  document.getElementById('root')
+);
