94bc520a0cd9ef677eb44bb36ee8420b62dae181
Mikolaj Snochowski <snochopl@gmail.com>
2020-02-24 20:14:58 +0100

    Addded composeEnhancers function

--- src/redux/index.js
+++ src/redux/index.js
@@ -6,7 +6,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  composeEnhancers()
+  composeEnhancers(applyMiddleware())
 );
 
 export default store;
