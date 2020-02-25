210997347142e10c5b22ccf8f05ae0b85bec1b69
Mikolaj Snochowski <snochopl@gmail.com>
2020-02-25 19:48:32 +0100

    Updated grid-template styles

--- src/components/styled-components/GridWrapper.js
+++ src/components/styled-components/GridWrapper.js
@@ -2,9 +2,8 @@ import styled from 'styled-components';
 
 const GridWrapper = styled.div`
   display: grid;
-  margin-top: 20px;
-  grid-template-columns: 1fr 100px 30px;
-  grid-gap: 0 10px;
+  grid-template-columns: 1fr .5fr 30px;
+  grid-gap: 20px 10px;
 `;
 
 export default GridWrapper;
